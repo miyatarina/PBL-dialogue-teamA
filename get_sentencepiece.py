b1c024f34a075df3111f84b3d3049f7cbecbf563
tomo2729 <115626382+tomo2729@users.noreply.github.com>
2022-12-14 00:25:35 +0900

    add docstring

--- get_sentencepiece.py
+++ get_sentencepiece.py
@@ -2,10 +2,23 @@
 import sentencepiece as spm
 import sys
 
-tag_list = ['<タグ>','<male>', '<female>']
+tag_list = []
 
 # 学習の実行
 def sentence_piece_train(file_path):
+    """
+    モデルの学習
+
+    Parameters
+    ----------
+    file_path : str
+        学習対象のテキスト
+    
+    Note
+    ----------
+        学習テキストはtag\tspeech\tresponse\nが前提
+        tagは事前に追加、学習データからは除去
+    """
     with open(file_path, "r", encoding="utf-8") as fin:
         with open(file_path.replace(".txt", ".detab.txt"), "w", encoding="utf-8") as fout:
             for line in fin:
