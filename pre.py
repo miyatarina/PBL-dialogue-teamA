b6b6972cacb8625e06b4ad9e1acab389f9e2a9de
tomo2729 <115626382+tomo2729@users.noreply.github.com>
2023-01-19 21:24:00 +0900

    Update pre.py

--- pre.py
+++ pre.py
@@ -1,14 +1,5 @@
 import random
-import tqdm
 import os
-
-def rand_ints_nodup(a, b, k):
-  ns = []
-  while len(ns) < k:
-    n = random.randint(a, b)
-    if not n in ns:
-      ns.append(n)
-  return ns
 
 def is_space(text):
     if " " in text or "　" in text:
@@ -24,13 +15,19 @@ def count_min_word(text, text_pair):
     else:
         return text_pair_count
 
-filesize = os.path.getsize("pre_data.txt")
+def rand_ints_nodup(a, b, k):
+  ns = []
+  while len(ns) < k:
+    n = random.randint(a, b)
+    if not n in ns:
+        print(len(ns))
+        ns.append(n)
+  return ns
+
 speech_list =[]
 response_list = []
 
-with open('pre_data.txt', "r", encoding='utf-8') as fin:
-    read_size = 0
-    with tqdm.tqdm(total=filesize) as pbar:
+with open("pre_data.txt", "r", encoding='utf-8') as fin:
         for line in fin:
             try:
                 speech, response = line.strip().split("\t")
@@ -42,9 +39,7 @@ with open('pre_data.txt', "r", encoding='utf-8') as fin:
                 continue
             speech_list.append(speech)
             response_list.append(response)
-            read_size += len(line.encode('utf-8'))
-            pbar.update(read_size)
 
 with open("100m.txt", "w", encoding='utf-8') as fout:
-    for i in tqdm.tqdm(rand_ints_nodup(0, len(speech_list), 1000000)):
+    for i in rand_ints_nodup(0, len(speech_list), 1000000):
         fout.write(speech[i] + '\t' + response_list[i]) 
