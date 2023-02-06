94673d7505495b0f50b1ea74e1ff65ab7904db31
KondoRisa <115621194+KondoRisa@users.noreply.github.com>
2023-02-06 18:02:45 +0900

    Add files via upload

---
+++ code/mycollectPMI.sh
@@ -0,0 +1,4 @@
+#SAVE_FILEを指定# #pyの中のwith openを経由しないとファイルが出力されない→ちゃんと待つ
+# SAVE_FILE="mycollectPMIlocate_todouhuken_si_description.txt"
+SAVE_FILE="collect_data_ex.txt"
+python python/myPMIlocate_city.py -sf $SAVE_FILE
