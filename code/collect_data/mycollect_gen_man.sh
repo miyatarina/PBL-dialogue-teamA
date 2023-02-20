09a26a332cac8c387c00cbefa7aadd5fdddafdcf
miyatarina <0yo3tu0no5ha@gmail.com>
2023-02-20 16:39:40 +0900

    collect_testdata.pyでタグが後ろに付与されるように変更

--- code/collect_data/mycollect_gen_man.sh
+++ code/collect_data/mycollect_gen_man.sh
@@ -1,3 +1,12 @@
 #SAVE_FILEを指定
-SAVE_FILE="mycollect_gen_man.txt"
-python python/mycollect_gen_man.py -sf $SAVE_FILE
+# SAVE_FILE="mycollect_gen_man.txt"
+# python python/mycollect_gen_man.py -sf $SAVE_FILE
+
+# ↓ここから編集
+SAVE_FILE=$1
+if [ -z "$SAVE_FILE" ]; then
+    echo "入力方法:bash collect_testdata.sh [出力させたいファイルパス]"
+    exit 0
+fi
+echo "出力ファイルパス:${SAVE_FILE}"
+python collect_testdata.py -sf "${SAVE_FILE}"
