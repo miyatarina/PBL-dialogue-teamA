d1d55160a6ff23292f1bd9c5bd51d78e3b1940b6
miyatarina <0yo3tu0no5ha@gmail.com>
2023-02-20 17:22:34 +0900

    mycollect_get_woman.shで引数で出力ファイル名を指定できるように変更

--- code/collect_data/mycollect_gen_woman.sh
+++ code/collect_data/mycollect_gen_woman.sh
@@ -1,3 +1,12 @@
-#SAVE_FILEを指定
-SAVE_FILE="mycollect_gen_woman.txt"
-python python/mycollect_gen_woman.py -sf $SAVE_FILE
+# #SAVE_FILEを指定
+# SAVE_FILE="mycollect_gen_woman.txt"
+# python python/mycollect_gen_woman.py -sf $SAVE_FILE
+
+# ↓ここから編集
+SAVE_FILE=$1
+if [ -z "$SAVE_FILE" ]; then
+    echo "入力方法:bash mycollect_gen_woman.sh [出力させたいファイルパス]"
+    exit 0
+fi
+echo "出力ファイルパス:${SAVE_FILE}"
+python mycollect_gen_woman.py -sf "${SAVE_FILE}"
