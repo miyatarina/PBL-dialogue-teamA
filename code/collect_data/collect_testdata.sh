58148eee467f785ff277829225df2b72c98a53cb
miyatarina <0yo3tu0no5ha@gmail.com>
2023-02-20 14:35:56 +0900

    collect_testdata.shで引数に出力ファイル名を指定できるように編集

--- code/collect_data/collect_testdata.sh
+++ code/collect_data/collect_testdata.sh
@@ -5,4 +5,10 @@
 
 # ↓ここから編集
 # パス名を置き換えてください
-python python/collect_testdata.py -sf "/home/miyata/PBL-dialogue-teamA/${1}"
+SAVE_FILE=$1
+if [ -z "$SAVE_FILE" ]; then
+    echo "入力方法:bash collect_testdata.sh [出力させたいファイル名]"
+    exit 0
+fi
+echo "出力ファイル名:${SAVE_FILE}"
+python collect_testdata.py -sf "/home/miyata/PBL-dialogue-teamA/data/${SAVE_FILE}"
